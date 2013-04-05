0f047a1276fd1fac7848d5c66a7b1a4caebbec59
KenTiN <quentin.lebourgeois@gmail.com>
2013-04-05 20:53:43 +0200

    Ajout des configurations exemple des tests

---
+++ config/tests/quentin.py
@@ -0,0 +1,33 @@
+#encoding: utf-8
+
+"""
+Ce fichier est un exemple de fichier de configuration d'un test de benchmark
+
+Pour l'utiliser, il suffit de le lancer via la commande `run' du shell afs
+
+@author Quentin
+"""
+
+# configuration du test de benchmark
+
+# nom du test de benchmark (doit être identique au nom du fichier)
+name = "quentin"
+
+# commentaire éventuel sur le test
+comment = "Test de benchmark de Quentin"
+
+# modules de test à lancer
+modules = ["dd"]
+
+# liste des clients cibles du test
+clients = {
+    "raspberry": {"path": "/dev/zero", "times": 1}
+}
+
+# spécifications sur le système de fichier testé
+fs = {
+    "name": "rozofs",
+    "version": "0.1"
+}
+
+# fin de configuration du test
