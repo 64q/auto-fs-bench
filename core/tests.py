cd009814ff8ca6747e9004f320ad952c2ec70b38
KenTiN <quentin.lebourgeois@gmail.com>
2013-04-06 02:51:27 +0200

    Ajout du fichier contenant les fonctions pour les tests

---
+++ core/tests.py
@@ -0,0 +1,26 @@
+#encoding: utf-8
+
+"""
+Ce fichier contient des fonctions utilitaires pour la gestion des tests de bench
+
+
+@author Quentin
+"""
+
+import core.utils
+
+def save_files(moduledir, filename, client, threads_results):
+    """
+    Cette fonction créé les fichiers correspondant aux tests effectués du module
+    """
+
+    with open(filename, "ab") as csvfile:
+        for thread_id, thread_content in threads_results.iteritems():
+            # écriture de la ligne de résultat 'return'
+            core.utils.csv_write_line(csvfile, 
+                [client + "_" + thread_id, thread_content["return"]])
+
+            # création des fichiers de sortie du module de benchmark
+            for fn, ct in thread_content["files"].iteritems():
+                with open(moduledir + "/" + fn, "w") as fp:
+                    fp.write(ct)
