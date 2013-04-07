7266da1efdd6237879de0c9ff502a5f983e94f3e
OlivierB <olivier.oblin@gmail.com>
2013-04-07 15:00:50 +0200

    config

--- config/tests/olivier.py
+++ config/tests/olivier.py
@@ -17,7 +17,7 @@ name = "olivier"
 comment = "bash scripts test"
 
 # modules de test à lancer
-modules = ["dd", "cp"]
+modules = ["dd", "cp", "bonnie", "iozone"]
 
 # liste des clients cibles du test
 clients = {
