0f057dc771367b25e63ae4476ba013686fcc23f1
OlivierB <olivier.oblin@gmail.com>
2013-04-08 16:06:26 +0200

    config

--- config/server.py
+++ config/server.py
@@ -14,7 +14,10 @@ Pour l'utiliser, il suffit de l'importer dans les fichiers source.
 clients = dict(
     localhost = "127.0.0.1",
     servoliv = "192.168.0.50",
-    raspberry = "192.168.0.3"
+    raspberry = "192.168.0.3",
+    ptrans1 = "10.0.2.51",
+    ptrans2 = "10.0.2.53",
+    ptrans3 = "10.0.2.52"
 )
 
 # port d'envoi du serveur
