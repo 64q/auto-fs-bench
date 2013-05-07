04547f527e07abbd233f0b306e3c54b4541166cc
KenTiN <quentin.lebourgeois@gmail.com>
2013-05-07 19:33:28 +0200

    Ajout de TestsError pour les tests avant le run

--- core/errors.py
+++ core/errors.py
@@ -1,10 +1,10 @@
 #encoding: utf-8
 
-'''
+"""
 Ce fichier contient toutes les erreurs lançables par l'application
 
 @author: Quentin
-'''
+"""
 
 class CmdError(Exception):
     """
@@ -46,6 +46,17 @@ class ClientTransmissionError(Exception):
 class InvalidModuleError(Exception):
     """
     Exception lancée lorsque le module est invalide
+    """
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+class TestsError(Exception):
+    """
+    Exception lancée lorsque les tests échouent
     """
 
     def __init__(self, msg):
